912e54e42344e57c01326e1ca307950320e94946
jays <jays.woo@sk.com>
2014-04-21 14:00:58 +0900

    fixed setup.sh
    copy include git-flow-complete

--- setupConfig.sh
+++ setupConfig.sh
@@ -4,3 +4,6 @@ cp .bashrc ~/.bashrc
 cp .gitconfig ~/.gitconfig
 cp .vimrc ~/.vimrc
 cp .git-completion.sh ~/.git-completion.sh
+cp .git-flow-completion.sh ~/.git-flow-completion.sh
+
+source ~/.bashrc
