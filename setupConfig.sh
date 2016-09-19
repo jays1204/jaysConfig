217adc4b1042eaf79a04d491066b399e2093ab57
jays.woo <jays.woo@kakaocorp.com>
2016-09-19 11:44:18 +0900

    setup file 수정
    for zsh

--- setupConfig.sh
+++ setupConfig.sh
@@ -5,5 +5,7 @@ cp .gitconfig ~/.gitconfig
 cp .vimrc ~/.vimrc
 cp .git-completion.sh ~/.git-completion.sh
 cp .git-flow-completion.sh ~/.git-flow-completion.sh
+cp .git-flow-completion.zsh ~/.git-flow-completion.zsh
+cp .zshrc ~/.zshrc
 
 source ~/.bashrc
