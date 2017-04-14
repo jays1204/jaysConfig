0f64e3d03a983fa6def70e823e9cbf4cecb3c5f2
jays.woo <jays.woo@kakaocorp.com>
2017-04-14 23:52:53 +0900

    add key repeat config
    add it

--- setupConfig.sh
+++ setupConfig.sh
@@ -8,4 +8,8 @@ cp .git-flow-completion.sh ~/.git-flow-completion.sh
 cp .git-flow-completion.zsh ~/.git-flow-completion.zsh
 cp .zshrc ~/.zshrc
 
+# key repeat 
+defaults write NSGlobalDomain KeyRepeat -float 1.7
+defaults write NSGlobalDomain InitialKeyRepeat -int 13
+
 source ~/.bashrc
